f5f522f40b6065b8114eeb0d0989f41a3eac5cf7
Jordan Beck <jordanfbeck@gmail.com>
2021-03-09 12:00:24 -0700

    add last stuff

--- app/controllers/comp_teams_controller.rb
+++ app/controllers/comp_teams_controller.rb
@@ -3,12 +3,14 @@ class CompTeamsController < ApplicationController
     # team = Competition.find(params[:competition_id]).teams.new(team_params)
     # require "pry"; binding.pry
     # team = Competition.find(params[:competition_id]).teams.new(:hometown, :nickname)
-    team = Competition.find(params[:competition_id])
-    team.teams.new
+    # team = Competition.find(params[:competition_id])
+    # team.teams.new
+    team = Team.new(hometown: params[:hometown], nickname: params[:nickname])
     # require "pry"; binding.pry
     team.save
     @competition = Competition.find(params[:competition_id])
     # require "pry"; binding.pry
-    redirect_to competition_path(params[:competition_id])
+    # redirect_to competition_path(params[:competition_id])
+    redirect_to competition_path(@competition)
   end
 end
